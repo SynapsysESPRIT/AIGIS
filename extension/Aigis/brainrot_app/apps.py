d3817e75d20010713e432b9caf06779581e9607a
taher <taher.ayadi@esprit.tn>
2025-04-27 19:42:39 +0100

    added nudity detection with blurring

--- extension/Aigis/brainrot_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BrainrotAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'brainrot_app'
--- extension/Aigis/brainrot_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('classify_video/', views.classify_video),
-]
